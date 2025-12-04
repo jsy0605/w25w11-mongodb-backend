c926d574f4b2d75d5c33a350883c121d536db6fc
jsy0605 <jungsy0605@naver.com>
2025-12-04 13:27:40 +0900

    ➕ Swagger 사용

--- src/main/kotlin/kr/ac/kumoh/s20201054/w25w11_mongodb_backend/contorller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20201054/w25w11_mongodb_backend/contorller/SongController.kt
@@ -7,10 +7,36 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/songs")
 @CrossOrigin(origins = ["http://localhost:5173"])
-class SongController (private val service: SongService){
+class SongController(
+    private val service: SongService
+) {
+    // Create
+    @PostMapping
+    fun addSong(@RequestBody song: Song): Song = service.addSong(song)
+
+    // Read (Retrieve)
     @GetMapping
     fun getAllSongs(): List<Song> = service.getAllSongs()
 
     @GetMapping("/{id}")
     fun getSongById(@PathVariable id: String): Song? = service.getSongById(id)
+
+    @GetMapping("/singer/{singer}")
+    fun getSongBySinger(@PathVariable singer: String): List<Song> = service.getSongBySinger(singer)
+
+    // Update
+    @PutMapping("/{id}")
+    fun updateSong(
+        @PathVariable id: String,
+        @RequestBody songDetails: Song
+    ): Song? =  service.updateSong(id, songDetails)
+
+    // Delete
+    @DeleteMapping("/{id}")
+    fun deleteSong(@PathVariable id: String): Map<String, String> {
+        return if (service.deleteSong(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
