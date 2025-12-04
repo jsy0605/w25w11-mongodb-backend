3b09814cafc8ae0f7aeeb882a365f22fe8bd0957
jsy0605 <jungsy0605@naver.com>
2025-12-04 13:48:47 +0900

    🚧CORS 추가

--- src/main/kotlin/kr/ac/kumoh/s20201054/w25w11_mongodb_backend/contorller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20201054/w25w11_mongodb_backend/contorller/SongController.kt
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/songs")
-@CrossOrigin(origins = ["http://localhost:5173"])
+@CrossOrigin(origins = [
+    "http://localhost:5173",
+    "https://jsy0605.github.io",
+])
 class SongController(
     private val service: SongService
 ) {
